d5c2997ccd33b8a2b6e895def3036e039adbddc5
Wictor Girardi <wictorgirardi@MacBook-Pro-de-Wictor-2.local>
2021-04-30 13:41:58 -0300

    criando service de request para o serviço

---
+++ movpass/lib/services/remote_services.dart
@@ -0,0 +1,17 @@
+import 'package:http/http.dart' as http;
+import 'package:movpass/models/services.dart';
+
+class RemoteServices {
+  static var client = http.Client();
+
+  static Future<List<Services>> fetchProducts() async {
+    var response = await client.get('https://dev.movpass.com.br/api/services/');
+    if (response.statusCode == 200) {
+      var jsonString = response.body;
+      return servicesFromJson(jsonString);
+    } else {
+      //show error message
+      return null;
+    }
+  }
+}
